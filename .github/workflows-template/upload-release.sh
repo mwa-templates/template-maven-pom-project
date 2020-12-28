0f08e5c8a95d73fb83d918b97a0771ec32d3e20d
Mirko Wagner <seelenvirtuose@gmail.com>
2020-12-28 13:35:46 +0100

    Template is ready!

--- .github/workflows-template/upload-release.sh
+++ .github/workflows-template/upload-release.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+REPO_OWNER=${GITHUB_REPOSITORY%/*}
+REPO_NAME=${GITHUB_REPOSITORY#*/}
+UPLOAD_URL=${UPLOAD_URL_TEMPLATE//%%REPO_OWNER%%/$REPO_OWNER}
+UPLOAD_URL=${UPLOAD_URL//%%REPO_NAME%%/$REPO_NAME}
+
 cd target/checkout
 mvn ${MVN_CLI_ARGS} --settings "${MAVEN_SETTINGS_FILE}" verify source:jar javadoc:jar deploy:deploy \
 		-Duploading \
