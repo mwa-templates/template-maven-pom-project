04499776e3fce835e8f2dc595c230a7635d0f734
Mirko Wagner <seelenvirtuose@gmail.com>
2021-01-01 16:58:08 +0100

    Diagnosis

--- .github/workflows-template/upload-release.sh
+++ .github/workflows-template/upload-release.sh
@@ -5,8 +5,12 @@ REPO_NAME=${GITHUB_REPOSITORY#*/}
 UPLOAD_URL=${UPLOAD_URL_TEMPLATE//%%REPO_OWNER%%/$REPO_OWNER}
 UPLOAD_URL=${UPLOAD_URL//%%REPO_NAME%%/$REPO_NAME}
 
+echo ${REPO_OWNER}
+echo ${REPO_NAME}
+echo ${UPLOAD_URL}
+
 cd target/checkout
-mvn ${MVN_CLI_ARGS} --settings "${MAVEN_SETTINGS_FILE}" verify source:jar javadoc:jar deploy:deploy \
+mvn ${MVN_CLI_ARGS} --settings "${MAVEN_SETTINGS_FILE}" -X -e verify source:jar javadoc:jar deploy:deploy \
 		-Duploading \
 		-DskipTests \
 		-DaltDeploymentRepository="${MAVEN_SERVER_ID}::${UPLOAD_URL}"
