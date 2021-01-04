6e578abbd54d57e9a653641b158d19140d983a88
Mirko Wagner <seelenvirtuose@gmail.com>
2021-01-04 11:27:29 +0100

    Quark change.

--- .github/workflows-template/upload-release.sh
+++ .github/workflows-template/upload-release.sh
@@ -12,4 +12,3 @@ mvn ${MVN_CLI_ARGS} --settings "${MAVEN_SETTINGS_FILE}" verify source:jar javado
 		-Duploading \
 		-DskipTests \
 		-DaltDeploymentRepository="${MAVEN_SERVER_ID}::${UPLOAD_URL}"
-		
